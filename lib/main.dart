754a4113a11ba1845b484529aea85dcf205b1e95
MasoudMaghsodi <magsodi333@gmail.com>
2024-05-14 20:13:09 +0330

    add font an localizations

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,72 +11,110 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        // Locale('en'), // English
+        Locale('fa'), // Spanish
+      ],
+      theme: ThemeData(
+        fontFamily: "dana",
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(
+            fontFamily: 'dana',
+            fontSize: 16,
+            fontWeight: FontWeight.w700,
+          ),
+          bodyLarge: TextStyle(
+            fontFamily: 'dana',
+            fontSize: 13,
+            fontWeight: FontWeight.w300,
+          ),
+          displayMedium: TextStyle(
+            fontFamily: 'dana',
+            fontSize: 14,
+            fontWeight: FontWeight.w300,
+          ),
+        ),
+      ),
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white70,
-          actions: [
-            const SizedBox(
-              width: 8,
+      home: const Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 243, 243, 243),
+      appBar: AppBar(
+        backgroundColor: Colors.white70,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SizedBox(
+              height: 37,
+              width: 37,
+              child: Image.asset("assets/images/risk.png"),
             ),
-            Expanded(
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: SizedBox(
-                  height: 23,
-                  width: 23,
-                  child: Image.asset("assets/images/menu.png"),
-                ),
+          ),
+          Text(
+            "قیمت به روز ارز",
+            style: Theme.of(context).textTheme.displayLarge,
+          ),
+          Expanded(
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: SizedBox(
+                height: 23,
+                width: 23,
+                child: Image.asset("assets/images/menu.png"),
               ),
             ),
-            const Text(
-              "قیمت به روز ارز",
-              style: TextStyle(color: Colors.black, fontSize: 20),
-            ),
-            const SizedBox(
-              width: 10,
+          ),
+          const SizedBox(
+            width: 8,
+          )
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(28.0),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  child: SizedBox(
+                    height: 30,
+                    width: 30,
+                    child: Image.asset("assets/images/q.png"),
+                  ),
+                ),
+                Text(
+                  "نرخ ارز آزاد چیست؟",
+                  style: Theme.of(context).textTheme.displayLarge,
+                ),
+              ],
             ),
             Padding(
-              padding: const EdgeInsets.only(right: 8),
-              child: SizedBox(
-                height: 37,
-                width: 37,
-                child: Image.asset("assets/images/risk.png"),
+              padding: const EdgeInsets.only(top: 12.0),
+              child: Text(
+                "نرخ ارزها در معاملات نقدی و رایج روزانه است معاملات نقدی معاملاتی هستند که خریدار و فروشنده به محض انجام معامله ارزو و ریال را با هم تبادل میکنند.",
+                style: Theme.of(context).textTheme.bodyLarge,
+                textDirection: TextDirection.rtl,
               ),
             ),
           ],
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  const Text(
-                    "نرخ ارز آزاد چیست؟",
-                    style: TextStyle(color: Colors.black, fontSize: 20),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 8.0),
-                    child: SizedBox(
-                      height: 30,
-                      width: 30,
-                      child: Image.asset("assets/images/q.png"),
-                    ),
-                  ),
-                ],
-              ),
-              const Padding(
-                padding: EdgeInsets.only(top: 8.0),
-                child: Text(
-                  "نرخ ارزها در معاملات نقدی و رایج روزانه است معاملات نقدی معاملاتی هستند که خریدار و فروشنده به محض انجام معامله ارزو و ریال را با هم تبادل میکنند.",
-                  style: TextStyle(color: Colors.black, fontSize: 16),
-                  textDirection: TextDirection.rtl,
-                ),
-              ),
-            ],
-          ),
         ),
       ),
     );
